b75c68d298d4ef2c1578f1484f07a3a105631a19
Sandani Zoysa <it20231200@my.sliit.lk>
2021-09-22 23:54:01 +0530

    modify login activity

--- app/src/main/java/com/example/travelbee/LoginActivity.java
+++ app/src/main/java/com/example/travelbee/LoginActivity.java
@@ -108,7 +108,7 @@ public class LoginActivity extends AppCompatActivity {
         notHaveAccountTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                startActivity(new Intent(LoginActivity.this, BudgetActivity.class));
                 finish();
             }
         });
@@ -218,7 +218,7 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             pd.dismiss();
                             FirebaseUser user = mAuth.getCurrentUser();
-                            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                            startActivity(new Intent(LoginActivity.this,DashboardActivity.class));
                             finish();
 
                         } else {
