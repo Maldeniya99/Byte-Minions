1053cfaaccfa90f0850dba50d5f12f89d90801c0
Kavindu Shehan <86120540+kavindushehan@users.noreply.github.com>
2021-09-23 21:40:11 +0530

    Updated

--- app/src/main/java/com/example/travelbee/LoginActivity.java
+++ app/src/main/java/com/example/travelbee/LoginActivity.java
@@ -305,7 +305,7 @@ public class LoginActivity extends AppCompatActivity {
 
                             Toast.makeText(LoginActivity.this, ""+user.getEmail(),Toast.LENGTH_SHORT).show();
                             //go to profile activity after logged in
-                            startActivity(new Intent(LoginActivity.this,notesActivity.class));
+                            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
                             finish();
                             //updateUI(user);
                         } else {
